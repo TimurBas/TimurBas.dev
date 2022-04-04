a46729ac42901477150caf0248fc538aacf4dcbd
timurbas <timurbas1@hotmail.com>
2022-04-04 22:12:53 +0200

    added chakra icons

--- components/navigation/ThemeButton.tsx
+++ components/navigation/ThemeButton.tsx
@@ -1,9 +1,12 @@
 import { Box } from '@chakra-ui/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 
 const ThemeButton = () => {
     return (
         <Box>
             This is the box
+            <MoonIcon></MoonIcon>
+            <SunIcon></SunIcon>
         </Box>
     )
 }
